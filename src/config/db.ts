8f4b6434cd5972e4a610ebf7c19b173d9693d688
Gustavo <gustavolopez819@gmail.com>
2025-03-11 20:15:12 -0500

    commit

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,7 @@ import { Sequelize } from 'sequelize-typescript'
 
 dotenv.config()
 
-export const db = new Sequelize( 'postgresql://test_db_fulx_user:JsrszwUH6JaGeG9nI3Ph6Ef6l5VYI07o@dpg-cuqtnvl6l47c73cikbf0-a.oregon-postgres.render.com/test_db_fulx', {
+export const db = new Sequelize( 'bd_url', {
     models: [__dirname + '/../models/**/*'],
     logging: false,
     dialectOptions: {
